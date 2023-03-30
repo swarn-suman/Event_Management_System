a32cad33f9ba9712ef16d0b9fd78e25898949126
Swarn <swarnsuman004@gmail.com>
2023-03-30 21:50:09 +0530

    Final Version

---
+++ book_msg.php
@@ -0,0 +1,18 @@
+<div class="container-fluid">
+	<p>Thank You for registration.We will further contct you for the confirmation of the booking.</p>
+</div>
+<div class="modal-footer display">
+	<div class="row">
+		<div class="col-md-12">
+			<button class="btn btn-secondary float-right" type="button" data-dismiss="modal">Close</button>
+		</div>
+	</div>
+</div>
+<style>
+	#uni_modal .modal-footer{
+		display: none;
+	}
+	#uni_modal .modal-footer.display{
+		display: block;
+	}
+</style>
